23c1cdcf2ee11468a9cbc00628592b71417ca93a
kali <kali@kali.kali>
2025-11-15 08:08:00 -0500

    1

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-
 def uppercase(str):
     for c in str:
-        # Kiçik hərfdirsə böyük hərfə çevir, yoxsa olduğu kimi çap et
-        print("{}".format(chr(ord(c) - 32) if 97 <= ord(c) <= 122 else c), end="")
+        print(
+            "{}".format(chr(ord(c) - 32) if 97 <= ord(c) <= 122 else c),
+            end=""
+        )
     print()
