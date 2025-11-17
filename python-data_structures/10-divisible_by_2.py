c00eba34b68a345844b67cc44611c8796fb97ba0
kali <kali@kali.kali>
2025-11-17 15:34:51 -0500

    1

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+c#!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     new_list = []
     for num in my_list:
---
+++ python-data_structures/11-delete_at.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    """Delete the item at a specific position in a list."""
+    if 0 <= idx < len(my_list):
+        my_list[:] = my_list[:idx] + my_list[idx+1:]
+    return my_list
