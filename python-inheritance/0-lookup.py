518f817d985c122ba9c95c330ce0d726b3188020
kali <kali@kali.kali>
2025-11-25 11:42:57 -0500

    1

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Function that returns a list of available attributes and methods of an object."""
+
+
+def lookup(obj):
+    """Return a list of available attributes and methods of an object."""
+    return dir(obj)
+
