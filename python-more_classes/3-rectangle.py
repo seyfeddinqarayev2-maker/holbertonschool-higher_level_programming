b3a26874a6beae0f953f89986289a8c96599c603
kali <kali@kali.kali>
2025-11-25 10:42:24 -0500

    1

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -46,3 +46,14 @@ class Rectangle:
         """Return the rectangle perimeter."""
         if self.__width == 0 or self.__height == 0:
             return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """Return the printable representation using '#' characters."""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+
+        rect = []
+        for _ in range(self.__height):
+            rect.append("#" * self.__width)
+        return "\n".join(rect)
