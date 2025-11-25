48d51d268f84893f09f75bd4e590146a6a0478af
kali <kali@kali.kali>
2025-11-25 10:52:32 -0500

    1

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -62,6 +62,6 @@ class Rectangle:
         return f"Rectangle({self.__width}, {self.__height})"
 
     def __del__(self):
-        """Print a message when an instance is deleted and decrement counter."""
+        """Print message when instance is deleted."""
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
