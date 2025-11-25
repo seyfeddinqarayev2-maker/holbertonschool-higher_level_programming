f592266ee601eedd56b96c2045951589420dd0e1
kali <kali@kali.kali>
2025-11-25 12:54:36 -0500

    1

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,22 +1,10 @@
 #!/usr/bin/python3
-"""
-Bu modul Square sinfini ehtiva edir,
-Rectangle-dən miras alınır.
-"""
 
+# Düzgün import
 Rectangle = __import__('8-rectangle').Rectangle
 
-
 class Square(Rectangle):
-    """Kvadrat sinfi, Rectangle-dən miras alır"""
-
     def __init__(self, size):
-        """Kvadratı size ilə yaradır"""
         self.integer_validator("size", size)
         self.__size = size
-        # Kvadrat olduğu üçün width və height eyni olur
         super().__init__(size, size)
-
-    def area(self):
-        """Kvadratın sahəsini qaytarır"""
-        return self.__size * self.__size
