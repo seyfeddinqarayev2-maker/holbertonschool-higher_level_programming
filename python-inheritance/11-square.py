3f55bec843e5b85e37d22447e526281a527cb480
kali <kali@kali.kali>
2025-11-25 13:05:55 -0500

    1

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -11,12 +11,17 @@ class Square(Rectangle):
     """Kvadrat sinfi, Rectangle-dən miras alır"""
 
     def __init__(self, size):
+        """Kvadratı size ilə yaradır"""
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(size, size)
 
     def area(self):
+        """Kvadratın sahəsini qaytarır"""
         return self.__size * self.__size
 
     def __str__(self):
-        return "[Square] {}/{}".format(self._Rect__width, self._Rect__height)
+        """Kvadratı [Square] <width>/<height> formatında string-ə"""
+        return "[Square] {}/{}".format(
+            self._Rectangle__width, self._Rectangle__height
+        )
