764f089cf149f72ea129ebb4803cbedff51f7379
kali <kali@kali.kali>
2025-11-28 12:54:03 -0500

    1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Module that defines a function to write a string to a UTF-8 text file."""
-     
+
 
 def write_file(filename="", text=""):
     """Writes a string to a UTF-8 text file and returns characters written."""
