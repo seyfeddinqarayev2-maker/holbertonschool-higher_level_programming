9e647acd899da41c7e345a9bf9a14a0d49a647f4
kali <kali@kali.kali>
2025-11-28 13:18:47 -0500

    1

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module that generates Pascal's triangle."""
+
+
+def pascal_triangle(n):
+    """Returns a list of lists of integers representing Pascal's triangle of n."""
+    if n <= 0:
+        return []
+
+    triangle = []
+
+    for i in range(n):
+        row = [1]
+        if i > 0:
+            prev_row = triangle[i - 1]
+            for j in range(1, i):
+                row.append(prev_row[j - 1] + prev_row[j])
+            row.append(1)
+        triangle.append(row)
+
+    return triangle
