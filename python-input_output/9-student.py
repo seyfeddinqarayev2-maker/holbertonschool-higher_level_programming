79bbf147bcdc33c462030086281a69ecca8f1b8f
kali <kali@kali.kali>
2025-11-28 13:15:02 -0500

    1

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -6,7 +6,8 @@ class Student:
     """Represents a student."""
 
     def __init__(self, first_name, last_name, age):
-        """Initialize a Student instance with first_name, last_name, and age."""
+        """Initialize a Student instance with first_name, last_name,
+        and age."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
