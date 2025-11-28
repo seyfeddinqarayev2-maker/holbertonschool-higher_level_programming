f369e6738e650cf0a8bedd7ad40b4265701f0e56
kali <kali@kali.kali>
2025-11-28 13:19:51 -0500

    1

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -3,7 +3,8 @@
 
 
 def pascal_triangle(n):
-    """Returns a list of lists of integers representing Pascal's triangle of n."""
+    """Returns a list of lists of integers representing
+    Pascal's triangle of n."""
     if n <= 0:
         return []
 
