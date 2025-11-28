07142fed6c469cd0d832859771679d75737e9d3e
kali <kali@kali.kali>
2025-11-28 18:55:46 -0500

    1

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -4,4 +4,4 @@ FROM tv_shows
 LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
 WHERE genres.show_id IS NULL
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC:
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
