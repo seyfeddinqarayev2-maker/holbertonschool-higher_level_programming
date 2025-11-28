034d4a497e23a820cb8c7114d81894019c27e1a3
kali <kali@kali.kali>
2025-11-28 13:15:57 -0500

    1

---
+++ python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Module that defines a Student class with optional attribute filtering."""
+
+
+class Student:
+    """Represents a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a Student instance with first_name, last_name,
+        and age."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return the dictionary representation of the Student instance.
+
+        If attrs is a list of strings, only attributes in this list are
+        retrieved. Otherwise, all attributes are retrieved.
+        """
+        if isinstance(attrs, list) and all(isinstance(a, str) for a in attrs):
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        return self.__dict__.copy()
