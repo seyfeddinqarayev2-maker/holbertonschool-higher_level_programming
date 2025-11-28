e8218bdb65e03528a7f030b3f056e4466f917651
kali <kali@kali.kali>
2025-11-28 13:09:42 -0500

    1

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Script that adds all arguments to a Python list and saves them to a JSON file."""
+
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+filename = "add_item.json"
+
+try:
+    items = load_from_json_file(filename)
+except FileNotFoundError:
+    items = []
+
+items.extend(sys.argv[1:])  # add all arguments except the script name
+save_to_json_file(items, filename)
