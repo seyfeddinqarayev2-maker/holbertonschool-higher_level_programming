1af28850d39f4df8be642ae66b9d16576589cc7e
kali <kali@kali.kali>
2025-11-28 13:13:10 -0500

    1

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-"""Module that returns a dictionary description of a class for JSON serialization."""
+"""Module that returns a dictionary description of a class instance
+for JSON serialization."""
 
 
 def class_to_json(obj):
-    """Returns the dictionary description with simple data structures of an object."""
+    """Returns the dictionary description with simple data structures
+of an object."""
     return obj.__dict__
