338ea59ae5f8ec43f59ffc99c3b50715ae3eee4d
kali <kali@kali.kali>
2025-11-28 13:10:46 -0500

    1

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -2,8 +2,12 @@
 """Script that adds all arguments to a Python list and saves them to a JSON file."""
 
 import sys
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__(
+    '5-save_to_json_file'
+).save_to_json_file
+load_from_json_file = __import__(
+    '6-load_from_json_file'
+).load_from_json_file
 
 filename = "add_item.json"
 
