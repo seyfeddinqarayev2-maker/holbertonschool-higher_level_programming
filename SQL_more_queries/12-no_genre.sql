f25372be5216cad59dae1a3e4f97a55cef1cd3ea
kali <kali@kali.kali>
2025-11-28 19:04:47 -0500

    1

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,7 +1,7 @@
 -- lll
-SELECT tv_shows.title, tv_show_genres.genre_id
+SELECT tv_shows.title
 FROM tv_shows
 LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
-WHERE genres.show_id IS NULL
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+WHERE tv_show_genres.show_id IS NULL
+ORDER BY tv_shows.title ASC;
